50ebf5289915ab89a37f96828dff0cce5d8fa82d
nogsposito <viniciussposito3103@gmail.com>
2025-06-07 17:22:05 -0300

    levando ao repositorio

---
+++ src/main/java/br/com/nogsposito/portfolio/comerciocais/ComerciocaisApplication.java
@@ -0,0 +1,13 @@
+package br.com.nogsposito.portfolio.comerciocais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComerciocaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComerciocaisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/nogsposito/portfolio/comerciocais/ComerciocaisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nogsposito.portfolio.comerciocais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComerciocaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
