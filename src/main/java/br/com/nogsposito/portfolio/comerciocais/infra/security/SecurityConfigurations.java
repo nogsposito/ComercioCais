a07bdcd7a07bf2333a7043a6052f9c86d47e068f
nogsposito <viniciussposito3103@gmail.com>
2025-06-11 14:26:26 -0300

    integracao back e front -> seguranca desabilitada

--- src/main/java/br/com/nogsposito/portfolio/comerciocais/infra/security/SecurityConfigurations.java
+++ src/main/java/br/com/nogsposito/portfolio/comerciocais/infra/security/SecurityConfigurations.java
@@ -2,24 +2,49 @@ package br.com.nogsposito.portfolio.comerciocais.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations {
-    
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .csrf(csrf -> csrf.disable()) // desabilita proteçao CSRF
+                .csrf(csrf -> csrf.disable())
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(req -> {
-                    req.requestMatchers("/**").permitAll();
-                    req.anyRequest().authenticated(); // POR ENQUANTO permite qualquer requisicao.
+                    req.requestMatchers(HttpMethod.GET, "/products").permitAll();
+                    req.requestMatchers(HttpMethod.GET, "/products/**").permitAll();
+                    
+                    req.requestMatchers(HttpMethod.POST, "/auth/login").permitAll();
+                    req.requestMatchers(HttpMethod.POST, "/auth/register").permitAll();
+                    
+                    req.anyRequest().authenticated();
                 })
                 .build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        
+        return source;
     }
 }
